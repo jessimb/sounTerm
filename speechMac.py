6acaab63622a87b99240209ac4dadc1f792f71b3
Jessi Beck <jessimb@umich.edu>
2015-01-17 09:54:49 -0500

    better doesnt use record

--- speechMac.py
+++ speechMac.py
@@ -1,5 +1,5 @@
 #import pyaudio
-"""import speech_recognition as sr
+import speech_recognition as sr
 r = sr.Recognizer()
 with sr.Microphone() as source:                # use the default microphone as the audio source
     audio = r.listen(source)                   # listen for the first phrase and extract it into audio data
@@ -7,8 +7,8 @@ with sr.Microphone() as source:                # use the default microphone as t
 try:
     print("You said " + r.recognize(audio))    # recognize speech using Google Speech Recognition
 except LookupError:                            # speech is unintelligible
-    print("Could not understand audio")"""
-import speech_recognition as sr
+    print("Could not understand audio")
+"""import speech_recognition as sr
 r = sr.Recognizer()
 with sr.WavFile("demo.wav") as source:              # use "test.wav" as the audio source
     audio = r.record(source)                        # extract audio data from the file
@@ -18,4 +18,9 @@ try:
 except KeyError:                                    # the API key didn't work
     print("Invalid API key or quota maxed out")
 except LookupError:                                 # speech is unintelligible
-    print("Could not understand audio")
+    print("Could not understand audio")"""
+"""from pygsr import Pygsr
+speech = Pygsr()
+speech.record(3) # duration in seconds (3)
+phrase, complete_response = speech.speech_to_text('es_ES') # select the language
+print phrase"""
